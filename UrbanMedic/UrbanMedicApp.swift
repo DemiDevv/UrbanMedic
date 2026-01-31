d0a18b07413a6b17487a44cd0e09dc7b770468dd
Demian Petropavlov <demyanpain@gmail.com>
2026-01-31 19:15:22 +0400

    Initial project setup

---
+++ UrbanMedic/UrbanMedicApp.swift
@@ -0,0 +1,17 @@
+//
+//  UrbanMedicApp.swift
+//  UrbanMedic
+//
+//  Created by Demain Petropavlov on 31.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct UrbanMedicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
